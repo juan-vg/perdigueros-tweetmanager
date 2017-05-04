6fb421026f3a8c711103da24fafe23bf238fb5b9
martafr <mfriaszapater@gmail.com>
2017-05-04 21:41:59 +0200

    Undo fix tabs on app.js and server.js & remove eclipse metadata

--- backend/server.js
+++ backend/server.js
@@ -12,40 +12,40 @@ mongoose.connect('mongodb://localhost:27017/ptm');
 
 //swagger definition
 var swaggerDefinition = {
-        info: {
-            title: 'API de gestión de usuarios',
-            version: '1.0.0',
-            description: 'Descripción del API del servicio de usuarios'
-        },
-        host: 'localhost:8888',
-        basePath: '/',
-};
+	info: {
+	title: 'API de gestión de usuarios',
+	version: '1.0.0',
+	description: 'Descripción del API del servicio de usuarios'
+	},
+	host: 'localhost:8888',
+	basePath: '/',
+	};
 
-//options for the swagger docs
+// options for the swagger docs
 var options = {
-        // import swaggerDefinitions
-        swaggerDefinition: swaggerDefinition,
-        // path to the API docs
-        apis: ['./app.js'],
+	// import swaggerDefinitions
+	swaggerDefinition: swaggerDefinition,
+	// path to the API docs
+	apis: ['./app.js'],
 };
 
-//initialize swagger-jsdoc
+// initialize swagger-jsdoc
 var swaggerSpec = swaggerJSDoc(options);
 
 //Acepta JSON y valores codificados en la propia URL
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ "extended": true }));
-
-//Todos los endpoint del API se encuentran en este fichero
+ 
+// Todos los endpoint del API se encuentran en este fichero
 var routes = require("./app.js")(app);
 
-//swagger
+// swagger
 app.get('/swagger.json', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
+	res.setHeader('Content-Type', 'application/json');
+	res.send(swaggerSpec);
 });
 
-//hace publica la carpeta "public"
+// hace publica la carpeta "public"
 app.use(express.static('./public'));
 
 var server = app.listen(8888, function () {
