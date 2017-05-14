969793b3aead1543091492a7e839dc0a608011e9
juan-vg <643821@unizar.es>
2017-05-14 17:56:43 +0200

    Apply Shared check for valid dbIDs on Hashtags module

--- backend/hashtags.js
+++ backend/hashtags.js
@@ -1,5 +1,6 @@
 var hashtagsModel = require("./models/hashtags");
-var verificator = require('./account-verifications.js');
+var accaccVerificator = require("./account-verifications");
+var dbaccVerificator = require("./db-verifications");
 var objectID = require('mongodb').ObjectID;
 
 //ADMIN BYPASS
@@ -7,57 +8,68 @@ exports.getAll = function (accountID, callback){
 
     var error, data;
     
-    // check if the token has access to twitterAccountId
-    verificator.verifyUser(accountID, function(success, reason){
-        
-        if(success){
-            
-            // get all hashtags that twitterAccountId owns
-            hashtagsModel.find({'twitterAccountId' : accountID.twitterAccountId},
-            
-                function(err,dbData){
-                    
-                    if(!err){
-                        error = false;
-                        
-                        // retrieve data
-                        if(dbData.length > 0){
-                            data = dbData;
-                        } else {
-                            //no results
-                            data = [];
-                        }
-                    } else {
-                        error = true;
-                        data = "DB ERROR";
-                    }
-                    
-                    callback(error, data);
-                }
-            );
-            
-        } else {
+    // Check if twitter-account ID is valid
+    dbVerificator.verifyDbId(accountID.twitterAccountId, function(success){
+		if(success){
+			// check if the token has access to twitterAccountId
+			accVerificator.verifyUser(accountID, function(success, reason){
+				
+				if(success){
+					
+					// get all hashtags that twitterAccountId owns
+					hashtagsModel.find({'twitterAccountId' : accountID.twitterAccountId},
+					
+						function(err,dbData){
+							
+							if(!err){
+								error = false;
+								
+								// retrieve data
+								if(dbData.length > 0){
+									data = dbData;
+								} else {
+									//no results
+									data = [];
+								}
+							} else {
+								error = true;
+								data = "DB ERROR";
+							}
+							
+							callback(error, data);
+						}
+					);
+					
+				} else {
+					
+					if(reason == "ACCOUNT NOT FOUND"){
+						console.log("HASHTAGS-GET-ALL: Twitter account NOT FOUND");
+
+						error = true;
+						data = "ACCOUNT NOT FOUND";
+					} else if(reason == "DB ERROR") {
+						console.log("HASHTAGS-GET-ALL: DB ERROR!!!" );
+
+						error = true;
+						data = "DB ERROR";
+					} else {
+						console.log("HASHTAGS-GET-ALL: User does not own that account");
+
+						error = true;
+						data = "FORBIDDEN" ;
+					}
+					
+					callback(error, data);
+				}
+			});
+		} else {
+			console.log("HASHTAGS-GET-ALL: Account ID is not valid");
 			
-			if(reason == "ACCOUNT NOT FOUND"){
-				console.log("HASHTAGS-GET-ALL: Twitter account NOT FOUND");
-
-				error = true;
-				data = "ACCOUNT NOT FOUND";
-			} else if(reason == "DB ERROR") {
-				console.log("HASHTAGS-GET-ALL: DB ERROR!!!" );
-
-				error = true;
-				data = "DB ERROR";
-			} else {
-				console.log("HASHTAGS-GET-ALL: User does not own that account");
-
-				error = true;
-				data = "FORBIDDEN" ;
-			}
-            
-            callback(error, data);
-        }
-    });
+			error = true;
+			data = "ACCOUNT ID NOT VALID";
+			callback(error, data);
+		}
+	});
 };
 
 //ADMIN BYPASS
@@ -65,58 +77,68 @@ exports.get = function (accountID, hashtag, callback){
     
     var error, data;
     
-    // check if the token has access to twitterAccountId
-    verificator.verifyUser(accountID, function(success, reason){
-        
-        if(success){
-            
-            // get the specified hashtag if twitterAccountId owns it
-            hashtagsModel.find({'twitterAccountId' : accountID.twitterAccountId, 'hashtag': hashtag},
-            
-                function(err,dbData){
-                    
-                    if(!err){
-                        
-                        // retrieve data
-                        if(dbData.length > 0){
-                            error = false;
-                            data = dbData;
-                        } else {
-                            error = true;
-                            data = "NOT FOUND";
-                        }
-                    } else {
-                        error = true;
-                        data = "DB ERROR";
-                    }
-                    
-                    callback(error, data);
-                }
-            );
-            
-        } else {
+    // Check if twitter-account ID is valid
+    dbVerificator.verifyDbId(accountID.twitterAccountId, function(success){
+		if(success){
+			// check if the token has access to twitterAccountId
+			accVerificator.verifyUser(accountID, function(success, reason){
+				
+				if(success){
+					
+					// get the specified hashtag if twitterAccountId owns it
+					hashtagsModel.find({'twitterAccountId' : accountID.twitterAccountId, 'hashtag': hashtag},
+					
+						function(err,dbData){
+							
+							if(!err){
+								
+								// retrieve data
+								if(dbData.length > 0){
+									error = false;
+									data = dbData;
+								} else {
+									error = true;
+									data = "NOT FOUND";
+								}
+							} else {
+								error = true;
+								data = "DB ERROR";
+							}
+							
+							callback(error, data);
+						}
+					);
+					
+				} else {
+					
+					if(reason == "ACCOUNT NOT FOUND"){
+						console.log("HASHTAGS-GET: Twitter account NOT FOUND");
+
+						error = true;
+						data = "ACCOUNT NOT FOUND";
+					} else if(reason == "DB ERROR") {
+						console.log("HASHTAGS-GET: DB ERROR!!!" );
+
+						error = true;
+						data = "DB ERROR";
+					} else {
+						console.log("HASHTAGS-GET: User does not own that account");
+
+						error = true;
+						data = "FORBIDDEN" ;
+					}
+					
+					callback(error, data);
+				}
+			}); 
+		} else {
+			console.log("HASHTAGS-GET-ALL: Account ID is not valid");
 			
-            if(reason == "ACCOUNT NOT FOUND"){
-				console.log("HASHTAGS-GET: Twitter account NOT FOUND");
-
-				error = true;
-				data = "ACCOUNT NOT FOUND";
-			} else if(reason == "DB ERROR") {
-				console.log("HASHTAGS-GET: DB ERROR!!!" );
-
-				error = true;
-				data = "DB ERROR";
-			} else {
-				console.log("HASHTAGS-GET: User does not own that account");
-
-				error = true;
-				data = "FORBIDDEN" ;
-			}
-            
-            callback(error, data);
-        }
-    }); 
-    
+			error = true;
+			data = "ACCOUNT ID NOT VALID";
+			callback(error, data);
+		}
+	});
 };
 
 //ADMIN BYPASS
@@ -124,74 +146,84 @@ exports.post = function (accountID, hashtag, callback){
     
     var error, data;
     
-    // check if the token has access to twitterAccountId
-    verificator.verifyUser(accountID, function(success, reason){
-        
-        if(success){
-            
-            // check if the specified hashtag already exists
-            hashtagsModel.find({'twitterAccountId' : accountID.twitterAccountId, 'hashtag': hashtag},
-                
-                function(err,dbData){
-                    
-                    if(!err){
-                        
-                        if(dbData.length > 0){
-                            // already exists
-                            error = true;
-                            data = "ALREADY EXISTS";
-                            callback(error, data);
-                        } else {
-                            // everything ok
-                            var dbHashtags = new hashtagsModel();
-                            dbHashtags.twitterAccountId = accountID.twitterAccountId;
-                            dbHashtags.hashtag = hashtag;
-                            
-                            
-                            // save new hashtag
-                            dbHashtags.save(function(err, result) {
-                                if(!err){
-                                    error = false;
-                                    data = null;
-                                    callback(error, data);
-                                } else {
-                                    error = true;
-                                    data = "DB ERROR";
-                                    callback(error, data);
-                                }
-                            });
-                        }
-                        
-                    } else {
-                        error = true;
-                        data = "DB ERROR";
-                        callback(error, data);
-                    }
-                }
-            );
-            
-        } else {
-            if(reason == "ACCOUNT NOT FOUND"){
-				console.log("HASHTAGS-POST: Twitter account NOT FOUND");
+    // Check if twitter-account ID is valid
+    dbVerificator.verifyDbId(accountID.twitterAccountId, function(success){
+		if(success){
+			// check if the token has access to twitterAccountId
+			accVerificator.verifyUser(accountID, function(success, reason){
+				
+				if(success){
+					
+					// check if the specified hashtag already exists
+					hashtagsModel.find({'twitterAccountId' : accountID.twitterAccountId, 'hashtag': hashtag},
+						
+						function(err,dbData){
+							
+							if(!err){
+								
+								if(dbData.length > 0){
+									// already exists
+									error = true;
+									data = "ALREADY EXISTS";
+									callback(error, data);
+								} else {
+									// everything ok
+									var dbHashtags = new hashtagsModel();
+									dbHashtags.twitterAccountId = accountID.twitterAccountId;
+									dbHashtags.hashtag = hashtag;
+									
+									
+									// save new hashtag
+									dbHashtags.save(function(err, result) {
+										if(!err){
+											error = false;
+											data = null;
+											callback(error, data);
+										} else {
+											error = true;
+											data = "DB ERROR";
+											callback(error, data);
+										}
+									});
+								}
+								
+							} else {
+								error = true;
+								data = "DB ERROR";
+								callback(error, data);
+							}
+						}
+					);
+					
+				} else {
+					if(reason == "ACCOUNT NOT FOUND"){
+						console.log("HASHTAGS-POST: Twitter account NOT FOUND");
 
-				error = true;
-				data = "ACCOUNT NOT FOUND";
-			} else if(reason == "DB ERROR") {
-				console.log("HASHTAGS-POST: DB ERROR!!!" );
+						error = true;
+						data = "ACCOUNT NOT FOUND";
+					} else if(reason == "DB ERROR") {
+						console.log("HASHTAGS-POST: DB ERROR!!!" );
 
-				error = true;
-				data = "DB ERROR";
-			} else {
-				console.log("HASHTAGS-POST: User does not own that account");
+						error = true;
+						data = "DB ERROR";
+					} else {
+						console.log("HASHTAGS-POST: User does not own that account");
 
-				error = true;
-				data = "FORBIDDEN" ;
-			}
-            
-            callback(error, data);
-        }
-    });     
-    
+						error = true;
+						data = "FORBIDDEN" ;
+					}
+					
+					callback(error, data);
+				}
+			});
+		} else {
+			console.log("HASHTAGS-GET-ALL: Account ID is not valid");
+			
+			error = true;
+			data = "ACCOUNT ID NOT VALID";
+			callback(error, data);
+		}
+	});
 };
 
 exports.put = function (accountID, hashtag, callback){
@@ -204,7 +236,7 @@ exports.put = function (accountID, hashtag, callback){
  * Function not needed. Wait for more data
  * 
     // check if the token has access to twitterAccountId
-    verifyUser(accountID, function(success){
+    accVerificator.verifyUser(accountID, function(success){
         
         if(success){
             
@@ -265,67 +297,77 @@ exports.delete = function (accountID, hashtag, callback){
     
     var error, data;
     
-    // check if the token has access to twitterAccountId
-    verificator.verifyUser(accountID, function(success, reason){
-        
-        if(success){
-            
-            // check if the specified hashtag exists
-            hashtagsModel.find({'twitterAccountId' : accountID.twitterAccountId, 'hashtag': hashtag},
-                
-                function(err,dbData){
-                    
-                    if(!err){
-                        
-                        if(dbData.length > 0){
-                            // existing hashtag -> delete hashtag
-                            hashtagsModel.remove({'_id': new objectID(dbData[0]._id)}, function(err, result) {
-                                if(!err){
-                                    error = false;
-                                    data = null;
-                                    callback(error, data);
-                                } else {
-                                    error = true;
-                                    data = "DB ERROR";
-                                    callback(error, data);
-                                }
-                            });
+    // Check if twitter-account ID is valid
+    dbVerificator.verifyDbId(accountID.twitterAccountId, function(success){
+		if(success){
+			// check if the token has access to twitterAccountId
+			accVerificator.verifyUser(accountID, function(success, reason){
+				
+				if(success){
+					
+					// check if the specified hashtag exists
+					hashtagsModel.find({'twitterAccountId' : accountID.twitterAccountId, 'hashtag': hashtag},
+						
+						function(err,dbData){
+							
+							if(!err){
+								
+								if(dbData.length > 0){
+									// existing hashtag -> delete hashtag
+									hashtagsModel.remove({'_id': new objectID(dbData[0]._id)}, function(err, result) {
+										if(!err){
+											error = false;
+											data = null;
+											callback(error, data);
+										} else {
+											error = true;
+											data = "DB ERROR";
+											callback(error, data);
+										}
+									});
 
-                        } else {
-                            // hashtag does not exist
-                            error = true;
-                            data = "NOT EXIST";
-                            callback(error, data);
-                        }
-                        
-                    } else {
-                        error = true;
-                        data = "DB ERROR";
-                        callback(error, data);
-                    }
-                }
-            );
-            
-        } else {
-            if(reason == "ACCOUNT NOT FOUND"){
-				console.log("HASHTAGS-DELETE: Twitter account NOT FOUND");
+								} else {
+									// hashtag does not exist
+									error = true;
+									data = "NOT EXIST";
+									callback(error, data);
+								}
+								
+							} else {
+								error = true;
+								data = "DB ERROR";
+								callback(error, data);
+							}
+						}
+					);
+					
+				} else {
+					if(reason == "ACCOUNT NOT FOUND"){
+						console.log("HASHTAGS-DELETE: Twitter account NOT FOUND");
 
-				error = true;
-				data = "ACCOUNT NOT FOUND";
-			} else if(reason == "DB ERROR") {
-				console.log("HASHTAGS-DELETE: DB ERROR!!!" );
+						error = true;
+						data = "ACCOUNT NOT FOUND";
+					} else if(reason == "DB ERROR") {
+						console.log("HASHTAGS-DELETE: DB ERROR!!!" );
 
-				error = true;
-				data = "DB ERROR";
-			} else {
-				console.log("HASHTAGS-DELETE: User does not own that account");
+						error = true;
+						data = "DB ERROR";
+					} else {
+						console.log("HASHTAGS-DELETE: User does not own that account");
 
-				error = true;
-				data = "FORBIDDEN" ;
-			}
-            
-            callback(error, data);
-        }
-    });   
-    
+						error = true;
+						data = "FORBIDDEN" ;
+					}
+					
+					callback(error, data);
+				}
+			});
+		} else {
+			console.log("HASHTAGS-GET-ALL: Account ID is not valid");
+			
+			error = true;
+			data = "ACCOUNT ID NOT VALID";
+			callback(error, data);
+		}
+	});
 };
