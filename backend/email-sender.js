e46227a800158351c900a770a2efc90916eb0bdb
juan-vg <643821@unizar.es>
2017-05-17 00:42:14 +0200

    remove unused code

--- backend/email-sender.js
+++ backend/email-sender.js
@@ -33,21 +33,3 @@ module.exports = function(params) {
         });
     };
 };
-/*
-// setup email data with unicode symbols
-let mailOptions = {
-    from: '"PTM - Perdigueros Tweet Manager" <zaratech.ptm@gmail.com>', // sender address
-    to: 'make_the_king@hotmail.com, maketheking@gmail.com', // list of receivers
-    subject: 'PTM TEST', // Subject line
-    text: 'Hello world ?', // plain text body
-    html: '<b>Hello world ?</b>' // html body
-};
-
-// send mail with defined transport object
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error);
-    }
-    console.log('Message %s sent: %s', info.messageId, info.response);
-});
-*/
