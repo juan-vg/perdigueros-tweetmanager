d8c541830831685106c9c7ccbc2f3a77f348251c
juan-vg <643821@unizar.es>
2017-05-19 01:06:05 +0200

    Fix comment

--- backend/app.js
+++ backend/app.js
@@ -2682,7 +2682,7 @@ var appRouter = function(app) {
 			function(err, data){
 				
 				if(!err){
-					console.log("APP-VERIFY-CAPTCHA: Verifying captcha response");
+					console.log("APP-VERIFY-CAPTCHA: OK");
 					
 					response.writeHead(200, {"Content-Type": "application/json"});
 					response.write(JSON.stringify({"responseCode": 0, "responseDesc": data}));
