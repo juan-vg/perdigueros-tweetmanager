1a291bf5f4d32bc75fe25642a3563370e60b841a
juan-vg <643821@unizar.es>
2017-05-18 02:15:01 +0200

    Fix comments

--- backend/tweets.js
+++ backend/tweets.js
@@ -195,7 +195,7 @@ exports.userTimeline = function (accountID, callback){
 							}
 							
 						} else {
-							console.log("TWEETS-PUBLISH: Twitter error");
+							console.log("TWEETS-USER-TIMELINE: Twitter error");
 							
 							error = true;
 							data = "TWITTER ERROR";
@@ -206,19 +206,19 @@ exports.userTimeline = function (accountID, callback){
 					
 				} else {
 					if(reason == "ACCOUNT NOT FOUND"){
-                        console.log("TWEETS-SCHEDULE: Twitter account NOT FOUND");
+                        console.log("TWEETS-USER-TIMELINE: Twitter account NOT FOUND");
 
                         error = true;
                         data = "ACCOUNT NOT FOUND";
                         
                     } else if(reason == "DB ERROR") {
-                        console.log("TWEETS-SCHEDULE: DB ERROR!!!" );
+                        console.log("TWEETS-USER-TIMELINE: DB ERROR!!!" );
 
                         error = true;
                         data = "DB ERROR";
                         
                     } else {
-                        console.log("TWEETS-SCHEDULE: User does not own that account");
+                        console.log("TWEETS-USER-TIMELINE: User does not own that account");
 
                         error = true;
                         data = "FORBIDDEN" ;
@@ -239,79 +239,6 @@ exports.userTimeline = function (accountID, callback){
 	});
 }
 /*
-
-// returns the scheduled tweets
-exports.scheduled = function (accountID, callback){
-
-    var error, data;
-    
-    // Check if twitter-account ID is valid
-    dbVerificator.verifyDbId(accountID.twitterAccountId, function(success){
-		
-        if(success){
-            // check if the token has access to twitterAccountId
-            accVerificator.verifyUser(accountID, function(success, data){
-                
-                if(success){
-					
-					// create auth set
-					var secret = {
-						consumer_key: data.consumerKey,
-						consumer_secret: data.consumerSecret,
-						access_token_key: data.accessToken,
-						access_token_secret: data.accessTokenSecret
-					}
-					var Twitter = new TwitterPackage(secret);
-					
-					Twitter.get('statuses/update', function(err, body){
-						
-						if(!err){
-							error = false
-							data= null;
-						} else {
-							console.log("TWEETS-PUBLISH: Twitter error");
-							
-							error = true;
-							data = "TWITTER ERROR";
-						}
-						
-						callback(error, data);
-					});
-					
-				} else {
-					if(reason == "ACCOUNT NOT FOUND"){
-                        console.log("TWEETS-SCHEDULE: Twitter account NOT FOUND");
-
-                        error = true;
-                        data = "ACCOUNT NOT FOUND";
-                        
-                    } else if(reason == "DB ERROR") {
-                        console.log("TWEETS-SCHEDULE: DB ERROR!!!" );
-
-                        error = true;
-                        data = "DB ERROR";
-                        
-                    } else {
-                        console.log("TWEETS-SCHEDULE: User does not own that account");
-
-                        error = true;
-                        data = "FORBIDDEN" ;
-                    }
-                    
-                    callback(error, data);
-				}
-				
-			});
-			
-		} else {
-			console.log("HASHTAGS-GET-ALL: Account ID is not valid");
-            
-            error = true;
-            data = "ACCOUNT ID NOT VALID";
-            callback(error, data);
-		}
-	});
-}
 
 // returns the home timeline (published tweets by followed users)
 exports.homeTimeline = function (accountID, callback){
@@ -386,6 +313,79 @@ exports.homeTimeline = function (accountID, callback){
 	});
 }
 /*
+// returns the scheduled tweets
+exports.scheduled = function (accountID, callback){
+
+    var error, data;
+    
+    // Check if twitter-account ID is valid
+    dbVerificator.verifyDbId(accountID.twitterAccountId, function(success){
+		
+        if(success){
+            // check if the token has access to twitterAccountId
+            accVerificator.verifyUser(accountID, function(success, data){
+                
+                if(success){
+					
+					// create auth set
+					var secret = {
+						consumer_key: data.consumerKey,
+						consumer_secret: data.consumerSecret,
+						access_token_key: data.accessToken,
+						access_token_secret: data.accessTokenSecret
+					}
+					var Twitter = new TwitterPackage(secret);
+					
+					Twitter.get('statuses/update', function(err, body){
+						
+						if(!err){
+							error = false
+							data= null;
+						} else {
+							console.log("TWEETS-PUBLISH: Twitter error");
+							
+							error = true;
+							data = "TWITTER ERROR";
+						}
+						
+						callback(error, data);
+					});
+					
+				} else {
+					if(reason == "ACCOUNT NOT FOUND"){
+                        console.log("TWEETS-SCHEDULE: Twitter account NOT FOUND");
+
+                        error = true;
+                        data = "ACCOUNT NOT FOUND";
+                        
+                    } else if(reason == "DB ERROR") {
+                        console.log("TWEETS-SCHEDULE: DB ERROR!!!" );
+
+                        error = true;
+                        data = "DB ERROR";
+                        
+                    } else {
+                        console.log("TWEETS-SCHEDULE: User does not own that account");
+
+                        error = true;
+                        data = "FORBIDDEN" ;
+                    }
+                    
+                    callback(error, data);
+				}
+				
+			});
+			
+		} else {
+			console.log("HASHTAGS-GET-ALL: Account ID is not valid");
+            
+            error = true;
+            data = "ACCOUNT ID NOT VALID";
+            callback(error, data);
+		}
+	});
+}
+
 // returns tweets containing mentions to the current twitter-account
 exports.mentions = function (accountID, callback){
 
