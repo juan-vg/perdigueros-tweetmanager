2db9bdc9d63af4c70a503a0ea3b974a6804830cf
Sergio Pedrero Benedi <sergiotecnologia92@gmail.com>
2017-05-24 01:10:08 +0200

    Improves on index.html and dashboard-controllers
    Added [name] information to navbar text 'Bienvenido [name]'.

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -2,61 +2,63 @@
  * Created by sergiopedrerobenedi on 18/5/17.
  */
 
-angular.module('app', ['ngRoute','vcRecaptcha','satellizer',
-    'ngStorage']);
+/**
+ * Instance the angular module 'app' and all the extern modules that it uses.
+ */
+angular.module('app', ['ngRoute', 'vcRecaptcha', 'satellizer',
+    'ngStorage', 'ngResource']);
 
+//variable for manage the main module
 var app = angular.module("app");
 
-
 // configure our routes
-app.config(function($routeProvider,$locationProvider,$authProvider) {
+app.config(function ($routeProvider, $locationProvider, $authProvider) {
 
     $routeProvider
 
     // route for the home page
-        .when('/' ,{
-            templateUrl : 'partials/login/signin.html',
-            controller  : 'signinCtrl',
-            resolve: function($localStorage, $location) {
-                if ($localStorage.token) {
-                    $location.url('/dashboard');
-                }
-            }
+        .when('/', {
+            templateUrl: 'partials/login/signin.html',
+            controller: 'signinCtrl'
         })
         // route for the signup page
         .when('/signup', {
-            templateUrl : 'partials/login/signup.html',
-            controller  : 'signupCtrl',
+            templateUrl: 'partials/login/signup.html',
+            controller: 'signupCtrl'
         })
         //route for the validate page
-        .when('/validate',{
-            templateUrl : 'partials/login/validate.html',
-            controller : 'validateCtrl'
+        .when('/validate', {
+            templateUrl: 'partials/login/validate.html',
+            controller: 'validateCtrl'
         })
-        //route for the validate page
-        .when('/remember',{
-            templateUrl : 'partials/login/validate.html',
-            controller : 'validateCtrl'
+        //route for the remember page
+        .when('/remember', {
+            templateUrl: 'partials/login/validate.html',
+            controller: 'validateCtrl'
         })
 
         //route for first-login page
-        .when('/first-login',{
-            templateUrl : 'partials/login/first-login.html',
-            controller : 'firstLoginCtrl'
+        .when('/first-login', {
+            templateUrl: 'partials/login/first-login.html',
+            controller: 'firstLoginCtrl'
         })
-        //route for dashboard page
-        .when('/dashboard',{
-            templateUrl : 'partials/dashboard/dashboard.html',
-            controller : 'mainPanelCtrl',
+        //route for dashboard page, token required
+        .when('/dashboard', {
+            templateUrl: 'partials/dashboard/dashboard.html',
+            controller: 'ShowHideUserMenu',
         })
 
         //route for forgot password
-        .when('/forgot-password',{
-            templateUrl : 'partials/login/forgot.html',
-            controller : 'forgotPasswdCtrl'
+        .when('/forgot-password', {
+            templateUrl: 'partials/login/forgot.html',
+            controller: 'forgotPasswdCtrl'
         })
+        .when('/logout', {
+            templateUrl: 'partials/login/signin.html',
+            controller: 'LogoutController'
+        }).otherwise('/home');
+    ;
     $locationProvider.html5Mode(true);
-
     /**
      *  Satellizer config
      */
@@ -67,18 +69,13 @@ app.config(function($routeProvider,$locationProvider,$authProvider) {
         clientId: '212919395883976',
         url: 'http://zaratech-ptm.ddns.net:8888/auth/facebook',
         authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
-        redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/'
+        redirectUri: 'http://localhost:8000'
     });
 
     $authProvider.google({
         clientId: '60622240890',
         url: 'http://zaratech-ptm.ddns.net:8888/auth/google',
         authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
-        redirectUri : window.location.origin || window.location.protocol + '//' + window.location.host
+        redirectUri: window.location.origin || window.location.protocol + '//' + window.location.host
     });
-
-
 });
-
-
-
--- frontend/app/scripts/controllers.js
+++ frontend/app/scripts/controllers.js
@@ -1,16 +1,19 @@
 /**
  * Created by sergiopedrerobenedi on 18/5/17.
  */
-// create the controller and inject Angular's $scope
+// Create the principal module that contains all the services and controllers which management the application
 var app = angular.module('app');
 
-app.controller('signinCtrl',  ['$auth','$location','$scope','$http',
-    function($auth,$location,$scope,$http) {
-        if(localStorage.getItem("token")){
+/**
+ * Controller that manage the signin view,that is the main view of the application.
+ * Uses services $auth for satellizer, $location for routes, $scope for the scope and $http for internet services.
+ */
+app.controller('signinCtrl', ['$auth', '$location', '$scope', '$http',
+    function ($auth, $location, $scope, $http) {
+        if (localStorage.getItem("token")) {
             $location.url('/dashboard');
-            $scope.menuState.show = true;
         }
-        else{
+        else {
             $scope.login = function () {
                 var data = {
                     'email': $scope.email,
@@ -45,194 +48,185 @@ app.controller('signinCtrl',  ['$auth','$location','$scope','$http',
                     });
             }
 
-            };
+        }
+        ;
 
-            $scope.authenticate = function (provider) {
-                $auth.authenticate(provider)
-                //succesful authentication
-                    .then(function () {
-                        $location.url('/dashboard');
-                    })
-                    //handle errors
-                    .catch(function (error) {
-                        if (error.message) {
-                            console.log(error);
+        $scope.authenticate = function (provider) {
+            $auth.authenticate(provider)
+            //succesful authentication
+                .then(function () {
+                    $location.url('/dashboard');
+                })
+                //handle errors
+                .catch(function (error) {
+                    if (error.message) {
+                        console.log(error);
 
-                        } else if (error.data) {
-                            console.log(error);
+                    } else if (error.data) {
+                        console.log(error);
 
-                        } else {
-                            console.log(error);
-                        }
-                    });
-            };
+                    } else {
+                        console.log(error);
+                    }
+                });
+        };
+
     }]);
 
-//create the singup controller, if success redirection to /validate, else
-app.controller('signupCtrl', ['$scope','$http','$location',function($scope,$http,$location ) {
-    $scope.postData = function(){
-        app.value();
+/**
+ * Create the signup controller. Once it is successfully executed, redirects to validate view.
+ * Otherwise, treat posibble errors.
+ */
+
+app.controller('signupCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    $scope.postData = function () {
         var data = {
             'name': $scope.name,
             'surname': $scope.surname,
             'email': $scope.email,
-            'g-recaptcha-response' : $scope.captchaResponse
+            'g-recaptcha-response': $scope.captchaResponse
         };
-        $http.post('http://zaratech-ptm.ddns.net:8888/login/signup',data
+        $http.post('http://zaratech-ptm.ddns.net:8888/login/signup', data
         )
-            //200 status code(valid,user not exists and not have other problems)
-            .then(function(response){
-                //redirection to /validate view
-                $location.url('/validate');
-        },
-            //status code errors
-            function(response){
-                //if email address already in use
-                if(response.status==409) {
+        //200 status code(valid,user not exists and not have other problems)
+            .then(function (response) {
+                    //redirection to /validate view
+                    $location.url('/validate');
+                },
+                //status code errors
+                function (response) {
+                    //if email address already in use
+                    if (response.status == 409) {
 
-                }
-                //if captcha validation error
-                else if(response.status==400){
-                    console.log("Captcha validation error");
-                }
-                //db error
-                else if(response.status==500){
-                    console.log("DB error");
-                }
-            });
+                    }
+                    //if captcha validation error
+                    else if (response.status == 400) {
+                        console.log("Captcha validation error");
+                    }
+                    //db error
+                    else if (response.status == 500) {
+                        console.log("DB error");
+                    }
+                });
     };
 }]);
 
-//create the validateCtrl, verify the e-mail and the code and redirect to 'first-login' page
-app.controller('validateCtrl', ['$scope','$http','$location',function($scope,$http,$location) {
+/**
+ * Create the validateCtrl, verify the e-mail and the code and redirect to 'first-login' page
+ */
+app.controller('validateCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
     var mail = "";
-    $scope.validate = function(){
+    $scope.validate = function () {
         var data = {
             'email': $scope.email,
-            'code' : $scope.code
+            'code': $scope.code
         };
-        $http.post('http://zaratech-ptm.ddns.net:8888/login/validate',data
-        ).then(function(response){
+        $http.post('http://zaratech-ptm.ddns.net:8888/login/validate', data
+        ).then(function (response) {
                 console.log(response);
                 $location.url('/first-login');
             },
             //status code errors
-            function(response){
+            function (response) {
                 //incorrect validation code
-                if(response.status==401) {
+                if (response.status == 401) {
                     $scope.errorMessage = "Incorrect validation code";
                     console.log("Incorrect validation code");
                     $location.url('/');
                 }
                 //db error
-                else if(response.status==500){
+                else if (response.status == 500) {
                     console.log("DB error");
                 }
             });
     };
 
     // RESEND FUNCTION
-    $scope.resend = function(){
+    $scope.resend = function () {
         var data = {
             'email': $scope.emailresend
         };
-        $http.post('http://zaratech-ptm.ddns.net:8888/login/validate/resend',data
-        ).then(function(response){
+        $http.post('http://zaratech-ptm.ddns.net:8888/login/validate/resend', data
+        ).then(function (response) {
                 console.log(response);
                 $location.url('/validate');
             },
             //status code errors
-            function(response){
+            function (response) {
                 //already validated, not active or not existing mail for user
-                if(response.status==409) {
+                if (response.status == 409) {
                     console.log("User already validated,not active or not existing email.");
                     $location.url('/');
                 }
                 //db error
-                else if(response.status==500){
+                else if (response.status == 500) {
                     console.log("DB error");
                 }
             });
     }
 }]);
 
-//create the firstLoginCtrl, verify the e-mail and update passwords
-app.controller('firstLoginCtrl', ['$scope','$http','$location',function($scope,$http,$location) {
-    $scope.firstlogin = function(){
+/**
+ * Create the firstLoginCtrl, verify the e-mail and update passwords
+ */
+app.controller('firstLoginCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    $scope.firstlogin = function () {
         var data = {
             'email': $scope.email,
-            'oldPasswd' : $scope.oldPasswd,
+            'oldPasswd': $scope.oldPasswd,
             'newPasswd': $scope.newPasswd
         };
-        $http.post('http://zaratech-ptm.ddns.net:8888/login/firstlogin',data
-        ).then(function(response){
+        $http.post('http://zaratech-ptm.ddns.net:8888/login/firstlogin', data
+        ).then(function (response) {
                 $location.url('/');
                 console.log("Change password correct");
             },
             //status code errors
-            function(response){
+            function (response) {
                 //incorrect validation user or non active user, or not existing email
-                if(response.status==409) {
+                if (response.status == 409) {
                     console.log("User not validated,not active, or not existing email");
                     $location.url('/');
                 }
                 //db error
-                else if(response.status==500){
+                else if (response.status == 500) {
                     console.log("DB error");
                 }
             });
     };
 }]);
 
-//create the firstLoginCtrl, verify the e-mail and update passwords
-app.controller('forgotPasswdCtrl', ['$scope','$http','$location',function($scope,$http,$location) {
-    $scope.remember = function(){
+/**
+ * Create the forgotPasswdCtrl, that manages the forgotView .
+ */
+app.controller('forgotPasswdCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    $scope.remember = function () {
         var data = {
             'email': $scope.emailForgot
         };
-        $http.post('http://zaratech-ptm.ddns.net:8888/login/remember',data
-        ).then(function(response){
+        $http.post('http://zaratech-ptm.ddns.net:8888/login/remember', data
+        ).then(function (response) {
                 $location.url('/');
             },
             //status code errors
-            function(response){
+            function (response) {
                 //incorrect validation user or non active user, or not existing email
-                if(response.status==409) {
+                if (response.status == 409) {
                     console.log("User not validated,not active, or not existing email");
                     $location.url('/');
                 }
                 //db error
-                else if(response.status==500){
+                else if (response.status == 500) {
                     console.log("DB error");
                 }
             });
     };
 }]);
 
-//create the firstLoginCtrl, verify the e-mail and update passwords
-app.controller('LogoutController',['$location','$scope',function($location){
+/**
+ * Create the LogoutController that manage the logout function of the site.
+ */
+app.controller('LogoutController', ['$location', '$scope', function ($location) {
     localStorage.clear();
     $location.url('/');
 }]);
-
-app.controller('ShowHideUserMenu',['$scope','$route',function($scope,$route) {
-    $scope.$on('$routeChangeSuccess', function() {
-        $scope.userVisible = false;
-        if(localStorage.getItem("token")){
-            $scope.userVisible = true;
-        }
-    });
-}]);
-
-
-
-
-
-
-
-
-
-
-
-
-
