404d6ca2d04ea5adbfb3443328b4c6189982c513
juan-vg <643821@unizar.es>
2017-08-23 03:29:24 +0200

    Improve XML error handling

--- backend/xml-validator.js
+++ backend/xml-validator.js
@@ -5,20 +5,25 @@ var w3c = require('w3c-validate').createValidator();
 exports.validate = function(dtd, xml_str, callback){
     
     var dtd_str = fs.readFileSync('./public/XML/' + dtd + '.dtd', 'utf8');
+    var success = true;
 
-    var xml = libxml.parseXml(xml_str);
+    try {
+        var xml = libxml.parseXml(xml_str);
 
-    xml = xml.setDtd(dtd_str);
+        xml = xml.setDtd(dtd_str);
 
-    w3c.validate(xml.toString(), function (err) {
+        w3c.validate(xml.toString(), function (err) {
+            
+            if (err) {
+                success = false;
+            }
+            
+            callback(success);
+        });
         
-        var success = true;
-        
-        if (err) {
-            success = false;
-        }
-      
+    } catch(err) {
+        success = false;
         callback(success);
-    });
+    }
 };
 
